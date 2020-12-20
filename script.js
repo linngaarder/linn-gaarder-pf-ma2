589778e0c526e639b53a6b57df8036a04abae77b
linngaarder <71817586+linngaarder@users.noreply.github.com>
2020-12-20 13:55:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+// Question 1
+
+var petArray = ["cat", "cow", "dog"];
+
+// Question 2
+
+var petArray = ["cat", "cow", "dog"];
+console.log(petArray[1]);
+
+// Question 3
+
+var petArray = ["cat", "cow", "dog"];
+petArray.push("sheep");
+
+// Question 4
+
+var petArray = ["cat", "cow", "dog"];
+petArray.push("sheep");
+console.log(petArray.length);
+
+// Question 5
+
+var catObject = {
+        name: "Ragnar",
+        colour: "Black",
+        age: 7
+    };
+
+// Question 6
+
+var catObject = {
+        name: "Ragnar",
+        colour: "Black",
+        age: 7
+    };
+console.log(catObject.age);
+
+// Question 7
+
+var catArray = [
+    {
+    name: "Ragnar",
+    colour: "Black",
+    age: 7  
+    }
+];
+
+// Question 8
+
+var catArray = [
+    {
+    name: "Ragnar",
+    colour: "Black",
+    age: 7  
+    }
+];
+console.log(catArray.length);
+for(var i=0; i < catArray.length; i++);{
+    console.log(i);
+}
+
+// Question 9
+
+function logToConsole(otter) {
+    console.log(otter);
+}
+
+// Question 10
+
+function logToConsole(otter) {
+    console.log(otter);
+}
+logToConsole("donkey");
